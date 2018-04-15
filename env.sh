511a259f468dbc35f72bf2e3b09d38b03760b405
bilguun0203 <bilguun0203@gmail.com>
2018-04-15 13:01:56 +0800

    Necessary files

---
+++ env.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+ARCH="x86_64"
+ROOTFS_VER="2018.04.01"
+ROOTFS_FN="archlinux-bootstrap-${ROOTFS_VER}-${ARCH}.tar.gz"
+ROOTFS_URL="http://mirrors.kernel.org/archlinux/iso/${ROOTFS_VER}/${ROOTFS_FN}"
+
+FRTCP_BLD="17121600"
+FRTCP_VER="1.22-1"
+FRTCP_FN="fakeroot-tcp-${FRTCP_VER}-${ARCH}.pkg.tar.xz"
+FRTCP_URL="https://github.com/yuk7/arch-prebuilt/releases/download/${FRTCP_BLD}/${FRTCP_FN}"
+
+PAC_PKGS="bzip2 coreutils diffutils gawk gcc-libs gettext grep gzip inetutils iproute2 iputils less man-db man-pages nano sed sudo tar vi vim wget which"
+
+if type curl >/dev/null 2>&1 ;then
+    DLR="curl"
+fi
+if type wget >/dev/null 2>&1;then
+    DLR="wget"
+fi
+if type aria2c >/dev/null 2>&1; then
+    DLR="aria2c -x4"
+fi
+
+
